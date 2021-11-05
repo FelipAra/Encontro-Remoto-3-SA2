4135e709e51241d8d3a5d83b171eca80cce51595
Felipe Araujo <fsantos.159852@gmail.com>
2021-11-05 14:49:22 -0300

    Atividade encontro remoto 3

---
+++ PessoaJurica.cs
@@ -0,0 +1,15 @@
+namespace cadastroPessoa
+
+{
+    public class PessoaJurica : Pessoa
+    {
+        public string cnpj { get; set; }
+
+        public string razaoSocial { get; set; }
+
+        public override void pagarImposto(float salario)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace cadastroPessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica pf = new PessoaFisica();
+            PessoaFisica novaPf = new PessoaFisica();
+            Endereco end = new Endereco();
+
+            end.logradouro = "Ayrton Senna";
+            end.numero = 100;
+            end.complemento = "";
+            end.enderecoComercial = false;
+
+            novaPf.endereco = end;
+            novaPf.cpf = "123456789";
+            novaPf.nome = "Pessoa Fisica";
+            novaPf.dataNascimento = new DateTime(2000, 06, 15);
+
+            Console.WriteLine($@"Rua: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}");
+            
+            bool idadeValida = pf.ValidarDataNascimento(novaPf.dataNascimento);
+
+            if (idadeValida == true) {
+
+                Console.WriteLine($"Cadastro aprovado!");
+                
+            } else {
+                Console.WriteLine($"Cadastro não aprovado!");
+                
+            }
+        }
+    }
+}
